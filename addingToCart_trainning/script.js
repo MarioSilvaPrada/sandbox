18e07f4a63b7587bfd40ac5cd8ac6f9503ef0b80
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-06-12 14:42:08 +0100

    updating

--- addingToCart_trainning/script.js
+++ addingToCart_trainning/script.js
@@ -1,10 +1,34 @@
 const addButton = document.querySelectorAll(".add");
 
-console.log(addButton); 
+let ul = document.querySelector(".cart-list");
+
+let total = document.querySelector(".total");
+
+let totalCost = 0;
 
 for (let i = 0; i < addButton.length; i++) {
   addButton[i].addEventListener("click", function(e) {
+    let li = document.createElement("LI");
 
-    console.log(e.target.previousElementSibling.innerHTML)
+    let productDescription = e.target.previousElementSibling.previousElementSibling.innerHTML;
+    let productPrice = e.target.previousElementSibling.innerHTML;
+
+    li.appendChild(
+      document.createTextNode(
+        `ITEM: ${productDescription}  COST: ${productPrice}`
+      )
+    );
+
+    ul.appendChild(li);
+
+    totalCost += parseInt(productPrice);
+
+    if (totalCost > 0) {
+      total.innerHTML = "";
+      total.appendChild(document.createTextNode(`TOTAL: ${totalCost}€`));
+    } else {
+      total.appendChild(document.createTextNode(`TOTAL: ${totalCost}€`));
+    }
+
   });
 }
